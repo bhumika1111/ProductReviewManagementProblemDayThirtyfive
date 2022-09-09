62b148c68bacd9ca37d8ccb49b5a5fc7fdfc1413
bhumika1111 <bhoyarpatil@434>
2022-09-09 15:37:48 +0530

    Added Management Program File

---
+++ Management.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace ProductReviewManagement
+{
+    public class Management
+    {
+        public void TopRecords(List<ProductReview> review)
+        {
+            var recordData = (from productReview in review
+                              orderby productReview.Rating descending
+                              select productReview).Take(3);
+        }
+
+        public void SelectedRecords(List<ProductReview> listProductReview)
+        {
+            var recordData = from productReviews in listProductReview
+                             where (productReviews.ProductID == 1 || productReviews.ProductID == 4 || productReviews.ProductID == 9)
+                             && productReviews.Rating > 3
+                             select productReviews;
+
+
+
+            foreach (var list in recordData)
+            {
+                Console.WriteLine("ProductID:- " + list.ProductID + " " + "UserID:- " + list.UserID
+                    + " " + "Rating:- " + list.Rating + " " + "Review:- " + list.Review + " " + "isLike:- " + list.isLike);
+            }
+        }
+        public void RetrieveCountOfRecords(List<ProductReview> listProductReview)
+        {
+            var recordData = listProductReview.GroupBy(y => y.ProductID).Select(x => new { ProductID = x.Key, Count = x.Count() });
+
+
+        }
+    }
+}
